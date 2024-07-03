e823d33697f740960faf66a366885bc4ad97577a
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-03 08:37:23 +0000

    woms->admin.py->Waiter

--- megameal/woms/admin.py
+++ megameal/woms/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
 
-admin.site.register(Waiter)
+
 admin.site.register(Hotal_Tables)
 admin.site.register(Floor)
+
+
+@admin.register(Waiter)
+class WaiterAdmin(admin.ModelAdmin):
+    fields = ('vendorId', 'name', 'username', 'password', 'phone_number', 'email', 'image', 'is_waiter_head', 'is_active',)
+    
+    list_display = ('name', 'phone_number', 'is_active', 'vendorId',)
+    list_filter = ('is_active', 'vendorId',)
+    search_fields = ('name', 'phone_number', 'email', 'username',)
+    ordering = ('vendorId', 'name', 'is_active',)
+    # show_facets = admin.ShowFacets.ALWAYS
