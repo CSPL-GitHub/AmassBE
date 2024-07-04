9fdcbba60aef10887cc7d8d11dcfc18c5bb756d5
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-04 12:24:42 +0000

    koms->model->Station Translation

--- megameal/koms/admin.py
+++ megameal/koms/admin.py
@@ -11,10 +11,10 @@ admin.site.register(Staff)
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
-    fields = ('vendorId', 'isStation', 'station_name', 'client_id', 'client_secrete', 'tag', 'color_code', 'key',)
+    fields = ('vendorId', 'isStation', 'station_name', 'station_name_ar', 'client_id', 'client_secrete', 'tag', 'color_code', 'key',)
     
-    list_display = ('station_name', 'isStation', 'vendorId',)
+    list_display = ('station_name', 'station_name_ar', 'isStation', 'vendorId',)
     list_filter = ('isStation', 'vendorId',)
-    search_fields = ('station_name',)
+    search_fields = ('station_name', 'station_name_ar',)
     ordering = ('vendorId', 'station_name', 'isStation',)
     # show_facets = admin.ShowFacets.ALWAYS
