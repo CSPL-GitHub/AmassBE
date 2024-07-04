4006a094f3242ec1e54432838871fedc0302c358
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-04 12:15:07 +0000

    koms->admin.py->Station

--- megameal/koms/admin.py
+++ megameal/koms/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
+
 
 admin.site.register(KOMSOrderStatus)
 admin.site.register(Order_content)
 admin.site.register(Order)
 admin.site.register(Staff)
-admin.site.register(Station)
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    fields = ('vendorId', 'isStation', 'station_name', 'client_id', 'client_secrete', 'tag', 'color_code', 'key',)
+    
+    list_display = ('station_name', 'isStation', 'vendorId',)
+    list_filter = ('isStation', 'vendorId',)
+    search_fields = ('station_name',)
+    ordering = ('vendorId', 'station_name', 'isStation',)
+    # show_facets = admin.ShowFacets.ALWAYS
