5a720fbe3198510fbd3a7229c5befa72f4e7c1d8
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-11 09:06:33 +0000

    order->order_helper.py->remove POS_setting and className check

--- megameal/order/order_helper.py
+++ megameal/order/order_helper.py
@@ -80,36 +80,22 @@ class OrderHelper():
             API_Messages.STATUS: API_Messages.ERROR,
             "msg": "Something went wrong"
         }
-        try:
-            # ++++ pick all the channels of vendor
-            try:
-                posSettings = POS_Settings.objects.get(VendorId_id=vendorId)
-            except POS_Settings.DoesNotExist:
-                coreResponse["msg"] = "POS settings not found"
-                return coreResponse,200
 
-            order=Order.objects.filter(vendorId_id=vendorId,id=data.get("orderId")).first()
+        try:
+            order = Order.objects.filter(vendorId_id=vendorId,id=data.get("orderId")).first()
+            
             if not order:
-                order=Order.objects.filter(vendorId_id=vendorId,externalOrderld=data.get("orderId")).first()
+                order = Order.objects.filter(vendorId_id=vendorId,externalOrderld=data.get("orderId")).first()
+            
             # ++++++---- Stage The Order
+            
             stagingPos = StagingIntegration()
+            
             stageOrder = stagingPos.updateOrderStatus(request=data)
+            
             if stageOrder[API_Messages.STATUS] == API_Messages.SUCCESSFUL:
-                # posService = globals()[posSettings.className]
-                # posResponse = posService.updateOrderStatus(response=data)
-                # if posResponse[API_Messages.STATUS] == API_Messages.SUCCESSFUL:
-                if data.get("updatePoint")!=UpdatePoint.WOOCOMERCE:
-                    if order.platform.className:
-                        platformService=globals()[order.platform.className]
-                        print(platformService)
-                        platformResponse = platformService.updateOrderStatus(request=data,vendorId=vendorId)
-                        return platformResponse,200
-                    else:
-                        return stageOrder,200
-                    # else:
-                    #     return posResponse,200
-                # else:
-                #     return posResponse,500
+                return stageOrder,200
+                    
             else:
                 print("Order Status Update Error in stage++++++++++++++++++")
                 return stageOrder,500
