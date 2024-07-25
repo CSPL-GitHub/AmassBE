de3bf7f35cda5d86c65c9a2d1020c79eb21ed427
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-25 09:50:44 +0000

    order->updateOrderStatusFromKOMS, womsCreateOrder remove

--- megameal/order/urls.py
+++ megameal/order/urls.py
@@ -6,8 +6,6 @@ from .views import *
 urlpatterns = [
     path('openOrder/',openOrder,name="openOrder"),
     path('addLineItem/',addLineItem,name='addLineItem'),
-    # path('updateOrderStatusFromKOMS/',updateOrderStatusFromKOMS,name='updateOrderStatusFromKOMS'), #not used now
-    # path('womsCreateOrder/',womsCreateOrder,name='womsCreateOrder'), #not used now
     path('loyaltypointscredit/', create_loyalty_points_credit_history), # for testing purpose
     path('completeorder/', complete_order), # for testing purpose
     path('change_customer_id_of_order/', change_customer_id_of_order)
