110ea5dbde22b40de9616c3c42e70801fb598231
abhijitwavhalcspl <abhijit.wavhal@consociatesolutions.com>
2024-07-30 09:49:26 +0000

    koms->signals

--- megameal/koms/apps.py
+++ megameal/koms/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class KomsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'koms'
+
+    def ready(self):
+        import koms.signals
